49da16ee2b4bf2c9c58ead77b68d5fa62f9b3a11
VincentZhou198933 <jsonvincent1989@gmail.com>
2014-01-28 10:48:37 +0800

    First time update python project to GitHub

---
+++ Demo.py
@@ -0,0 +1,16 @@
+'''
+Created on Jan 28, 2014
+
+@author: 498357
+'''
+class Test:
+   def __init__(self):
+      self.a = 1
+      self.b = 2
+      
+   def run(self):
+      print 'Demo for GitHub python project!!!'
+
+if __name__ == '__main__':
+    test = Test()
+    demo = test.run()
